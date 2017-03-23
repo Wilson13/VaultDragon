d47635f2530b9e1dd9f90550e62f76334ce3c353
Wilson13 <wilsonxin@hotmail.com>
2017-03-23 16:29:18 +0800

    Setup PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,17 @@
 module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   */
-  apps : [
-
-    // First application
-    {
-      name      : "VaultDragon API",
-      script    : "app.js",
-      
-    }
-  ],
-
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
-  deploy : {
-    production : {
-      user : "ubuntu",
-      host : "ec2-34-208-10-36.us-west-2.compute.amazonaws.com",
-	key  : "../AWS/wilson-key.pem",
-      ref  : "origin/master",
-      repo : "git@github.com:Wilson13/VaultDragon.git",
-      path : "/home/ubuntu/server/VaultDragon",
-      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.config.js"
+  apps: [{
+    name: 'VaultDragon API2',
+    script: 'index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-34-208-10-36.us-west-2.compute.amazonaws.com',
+      key: '../AWS/wilson-key.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:Wilson13/VaultDragon.git',
+      path: '/home/ubuntu/server/VaultDragon',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
 }
