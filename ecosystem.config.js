9b1e2873461287d4b31083f7c0ccea4ed7841541
Wilson <wilsonxin@hotmail.com>
2017-03-24 17:05:51 +0800

    Commit on windows

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     production: {
       user: 'ubuntu',
       host: 'ec2-34-208-10-36.us-west-2.compute.amazonaws.com',
-      key: '../AWS/wilson-key.pem',
+      key: 'wilson-key.pem',
       ref: 'origin/master',
       repo: 'git@github.com:Wilson13/VaultDragon.git',
       path: '/home/ubuntu/server/VaultDragon',
