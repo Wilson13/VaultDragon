b54b4bff694266c67623efb276a811b0a5d907f7
Wilson <wilsonxin@hotmail.com>
2017-03-24 18:52:21 +0800

    Modified ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [{
-    name: 'VaultDragon API2',
+    name: 'VaultDragon API',
     script: 'vault_api.js'
   }],
   deploy: {
     production: {
       user: 'ubuntu',
       host: 'ec2-34-208-10-36.us-west-2.compute.amazonaws.com',
-      key: 'wilson-key.pem',
+      key: 'D:/My Documents/Yuan Ze University/Dropbox/AWS/wilson-key.pem',
       ref: 'origin/master',
       repo: 'git@github.com:Wilson13/VaultDragon.git',
       path: '/home/ubuntu/server/VaultDragon',
