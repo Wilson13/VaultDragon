3034d4672ad5acda7c27ba017640d12878b2a06d
Wilson <wilsonxin@hotmail.com>
2017-03-26 16:19:25 +0800

    vault_dragon_server.js

--- vault_dragon_server.js
+++ vault_dragon_server.js
@@ -1,8 +1,37 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
 
 /*app.get('/', (req, res) => {
   res.send('Hello World!')
 })*/
-app.use(express.static('public'))
-app.listen(3000, () => console.log('Server running on port 3000'))
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
+//app.use(express.static('public'))
+//app.listen(3000, () => console.log('Server running on port 3000'))
