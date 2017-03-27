45ecb531a94c0344c3ffc96c89951ad60ebc5f6e
Wilson <wilsonxin@hotmail.com>
2017-03-27 21:02:54 +0800

    Added UPDATE in vault_dragon_server.js

--- vault_dragon_server.js
+++ vault_dragon_server.js
@@ -66,10 +66,10 @@ router.route('/object')
 				} else {
 					// If object is not null, object is updated.
 					if (object)
-						res.json({ message: 'Object updated! ' , 'Key' : object.key, 'Value' : req.body.value, 'Updated at' : Date.now() });
+						res.json({ message: 'Object updated! ' , 'Key' : object.key, 'Value' : req.body.value, 'Updated at' : Date.now().toISOString() });
 					// If object is null, new object is created.
 					else
-						res.json({ message: 'Object created! ' , 'Key' : + req.body.key, 'Value' : req.body.value, 'Created at' : Date.now() });
+						res.json({ message: 'Object created! ' , 'Key' : + req.body.key, 'Value' : req.body.value, 'Created at' : Date.now().toISOString() });
 				}
 			});
         }
