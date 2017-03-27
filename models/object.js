eb30aa99dd355e51b8ff7c3575b2ee4f93a983ca
Wilson <wilsonxin@hotmail.com>
2017-03-27 16:02:06 +0800

    Added getValidationResult in vault_dragon_server.js

--- models/object.js
+++ models/object.js
@@ -4,7 +4,9 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 var ObjectSchema   = new Schema({
     key: String,
-	value: String
+	value: String,
+	created_at: { type: Date },
+	updated_at: { type: Date }
 });
 
 module.exports = mongoose.model('Object', ObjectSchema);
