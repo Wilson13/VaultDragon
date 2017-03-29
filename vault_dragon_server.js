61a44240cdd517b92a0882b91c5fdf00f6d9f488
Wilson <wilsonxin@hotmail.com>
2017-03-29 10:56:21 +0800

    Modified vault_dragon_server.js to create new object for history purpose

--- vault_dragon_server.js
+++ vault_dragon_server.js
@@ -1,5 +1,11 @@
 // vault_dragon_server.js
 
+// Build a version controlled key-value store with a HTTP API we can query that from. The API needs to be able to:
+// 1. Accept a key(string) and value(some json blob/string) and store them. If an existing key is sent, the value should be updated
+// 2. Accept a key and return the corresponding latest value
+// 3. When given a key AND a timestamp, return whatever the value of the key at the time was.
+// Assume only GET and POST requests for simplicity.
+
 const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
@@ -33,21 +39,22 @@ var router = express.Router();              // get an instance of the express Ro
     next(); // make sure we go to the next routes and don't stop here
 });*/
 
-// test route to make sure everything is working (accessed at GET http://localhost:3000/api)
+// Test route to make sure everything is working (accessed at GET http://localhost:3000/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-// Routes for our API
-
-// on routes that end in /object
+// On routes that end in /object
 // ----------------------------------------------------
 router.route('/object')
 
-    // create a bear (accessed at POST http://localhost:3000/api/object)
+	/* 1. Accept a key(string) and value(some json blob/string) and store them. If an existing key is sent, the value should be updated */
+    // Create an object (accessed at POST http://localhost:3000/api/object)
     .post(function(req, res) {
 
+		// Make sure key and value are both not empty
 		req.checkBody("key", "Key cannot be empty").notEmpty();
+		req.checkBody("value", "Value cannot be empty").notEmpty();
 		var errors = req.validationErrors();
 		
 		// Validation resulted errors
@@ -55,7 +62,7 @@ router.route('/object')
 			res.send(errors);
 			return;
 		} else {
-			// Originally wanted to use the function below, but decide to just save new Object every time for history timestamps purpose.
+			// Originally wanted to use the function below, but decided to just save new Object every time for historical timestamps purpose.
 			// Find object and update it's value if it exists.
 			/*Object.findOneAndUpdate({key : req.body.key}, {value : req.body.value}, {upsert: true, new: true}, function (err, object) {
 				if (err){
@@ -69,6 +76,8 @@ router.route('/object')
 						res.json({ message: 'Object created! ' , 'Key' : + req.body.key, 'Value' : req.body.value, 'Created at' : new Date().toISOString() });
 				}
 			});*/
+			
+			// Create and save new Object every time for historical timestamps purpose.
 			var object = new Object();	// create a new instance of the Object model
 			object.key = req.body.key;  // set the object key (comes from the request)
 			object.value = req.body.value;	// set the object key-paired value (comes from the request)
@@ -82,7 +91,7 @@ router.route('/object')
         }
     })
 	
-	 // get all the object (accessed at GET http://localhost:3000/api/object)
+	 // Display all the objects (accessed at GET http://localhost:3000/api/object)
     .get(function(req, res) {
 		Object.find(function(err, object) {
             if (err)
@@ -92,21 +101,23 @@ router.route('/object')
         });
     });
 	
-// on routes that end in /object/:key
+// On routes that end in /object/:key
+// ----------------------------------------------------
 router.route('/object/:key')
 	
-	// get the object with that key (accessed at GET http://localhost:8080/api/object/:key)
+	/* 2. Accept a key and return the corresponding latest value */
+	// get the object with that key (accessed at GET http://localhost:3000/api/object/:key)
     .get(function(req, res) {
 		Object.findOne({ key : req.params.key }, function(err, object) {
             if (object)
                 res.json(object.value);
 			else
 				res.json( { message: 'No object with key \'' + req.params.key + '\' was found.' });
-        });
+        }).sort({ updatedAt : -1 });
     });
 
 // REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
+// All of our routes will be prefixed with /api
 app.use('/api', router);
 
 // START THE SERVER
@@ -115,4 +126,3 @@ app.listen(port);
 console.log('Magic happens on port ' + port);
 
 //app.use(express.static('public'))
-//app.listen(3000, () => console.log('Server running on port 3000'))
